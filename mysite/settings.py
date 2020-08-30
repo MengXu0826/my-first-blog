85fba566e5757c2f6890aeec871214380a89c335
meng0826 <981966945@qq.com>
2020-08-30 22:25:45 +0800

    TDD c2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xn*48)_9^(+w*)26aau$yz#@c&)i9v66)ccd)b4t1((faqw3i1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
 
 
 # Application definition
